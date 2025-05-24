7cb3dd7f11f3fdc870572d0178a0aede8d773982
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2025-05-24 22:45:48 +0600

    client link generated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://findjob-22996.web.app",
+    "https://techsoluitionbd.web.app",
   ],
   credentials: true,
   optionSuccessStatus: 200,
